d3f5be0ea84637d4c19091c64cc021d59f65afa6
heyuantao <he_yuan_tao@163.com>
2020-09-12 21:01:08 +0800

    continue

--- src/main/scala/util/F1070LogParser.java
+++ src/main/scala/util/F1070LogParser.java
@@ -1,7 +1,9 @@
 package util;
 
 
+import org.stringtemplate.v4.ST;
 import scala.Serializable;
+import sun.management.BaseOperatingSystemImpl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,45 +16,100 @@ import java.util.regex.Pattern;
  * 相应的样例文件放在resources目录中
  */
 public class F1070LogParser implements Serializable {
-    public static String regEx="((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
-    //public static String srcIpAddrRegEx = "SrcIPAddr(\\d+)=((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
-    public static String srcIpAddrRegEx  = "SrcIPAddr\\(\\d*\\)=((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
-    public static String distIpAddrRegEx = "DstIPAddr\\(\\d*\\)=((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
 
-    List<String> ipList = null;
+    public static String sourceIpAddressRegEx       = "SrcIPAddr\\(\\d*\\)=((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
+    public static String destinationIpAddressRegEx  = "DstIPAddr\\(\\d*\\)=((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
+    public static String sourcePortRegEx            = "SrcPort\\(\\d*\\)=(\\d*)";
+    public static String destinationPortRegEx       = "DstPort\\(\\d*\\)=(\\d*)";
+    public static String protocolRegEx              = "Protocol\\(\\d*\\)=(\\w*)";
 
-    public F1070LogParser(){
+
+    String sourceIp         = "";
+    String destinationIp    = "";
+    String sourcePort       = "";
+    String destinationPort  = "";
+    String protocol         = "";
+    String time             = "";
+
+    public F1070LogParser(){ }
+
+    private String parseSourceIp(String content){
+        Pattern p = Pattern.compile(F1070LogParser.sourceIpAddressRegEx);
+        Matcher m = p.matcher(content);
+        if(m.find()){
+            String matchedString = m.group(0);
+            String[] splitMatchedString = matchedString.split("=");
+            String ipString = splitMatchedString[1];
+            return ipString;
+        }else{
+            return "";
+        }
     }
 
-    public void parseSourceIp(String content){
-        //String sourceip = null;
-        Pattern p = Pattern.compile(F1070LogParser.srcIpAddrRegEx);
+    private String parseDestinationIp(String content){
+        Pattern p = Pattern.compile(F1070LogParser.destinationIpAddressRegEx);
         Matcher m = p.matcher(content);
-        while(m.find()){
-            System.out.println(m.group(0));
+        if(m.find()){
+            String matchedString = m.group(0);
+            String[] splitMatchedString = matchedString.split("=");
+            String ipString = splitMatchedString[1];
+            return ipString;
+        }else{
+            return "";
         }
-        //return sourceIp;
     }
 
-    public void parseDistIp(String content){
-        //String sourceip = null;
-        Pattern p = Pattern.compile(F1070LogParser.distIpAddrRegEx);
+    private String parseSourcePort(String content){
+        Pattern p = Pattern.compile(F1070LogParser.sourcePortRegEx);
         Matcher m = p.matcher(content);
-        while(m.find()){
-            System.out.println(m.group(0));
+        if(m.find()){
+            String matchedString = m.group(0);
+            String[] splitMatchedString = matchedString.split("=");
+            String ipString = splitMatchedString[1];
+            return ipString;
+        }else{
+            return "";
         }
-        //return sourceIp;
+    }
+
+    private String parseDestinationPort(String content){
+        Pattern p = Pattern.compile(F1070LogParser.destinationPortRegEx);
+        Matcher m = p.matcher(content);
+        if(m.find()){
+            String matchedString = m.group(0);
+            String[] splitMatchedString = matchedString.split("=");
+            String ipString = splitMatchedString[1];
+            return ipString;
+        }else{
+            return "";
+        }
+    }
+
+    private String parseProtocal(String content){
+        Pattern p = Pattern.compile(F1070LogParser.protocolRegEx);
+        Matcher m = p.matcher(content);
+        if(m.find()){
+            String matchedString = m.group(0);
+            String[] splitMatchedString = matchedString.split("=");
+            String ipString = splitMatchedString[1];
+            return ipString;
+        }else{
+            return "";
+        }
+    }
+
+    private String parseTime(String content){
+        String[] splitedString = content.split(" ");
+        return splitedString[0];
     }
 
     public F1070LogParser parseLine(String content){
-        ipList = new ArrayList<String>();
-
-        Pattern p = Pattern.compile(F1070LogParser.regEx);
-        Matcher m = p.matcher(content);
-        while (m.find()) {
-            String result = m.group();
-            ipList.add(result);
-        }
+        this.sourceIp = parseSourceIp(content);
+        this.destinationIp = parseDestinationIp(content);
+        this.sourcePort = parseSourcePort(content);
+        this.destinationPort = parseDestinationPort(content);
+        this.protocol = parseProtocal(content);
+        this.time = parseTime(content);
         return this;
     }
 
@@ -61,16 +118,17 @@ public class F1070LogParser implements Serializable {
      * @return
      */
     public Boolean isValid(){
-        if(ipList.size()!=3){
+        if(this.sourceIp.equals("")){
             return Boolean.FALSE;
-        }else{
-            return Boolean.TRUE;
         }
+        if(this.destinationIp.equals("")){
+            return Boolean.FALSE;
+        }
+
+
+        return Boolean.TRUE;
     }
 
-    public String getSourceIp(){
-        return ipList.get(0);
-    }
 
     /**
      * 功能测试代码，测试程序输出结果是否正确
@@ -80,9 +138,29 @@ public class F1070LogParser implements Serializable {
         String rawContent = "2020-08-31T12:07:36.000Z 172.16.103.12 F1070-B %%10FILTER/6/FILTER_ZONE_IPV4_EXECUTION: SrcZoneName(1025)=Trust;DstZoneName(1035)=Untrust;Type(1067)=ACL;SecurityPolicy(1072)=Trust-UnTrust;RuleID(1078)=2;Protocol(1001)=UDP;Application(1002)=dns;SrcIPAddr(1003)=172.17.6.24;SrcPort(1004)=40012;DstIPAddr(1007)=114.114.114.114;DstPort(1008)=53;MatchCount(1069)=1;Event(1048)=Permit;";
 
         F1070LogParser parser = new F1070LogParser();
-        //parser.parseLine(rawContent);
-        //System.out.println(parser.ipList);
-        //parser.parseSourceIp(rawContent);
-        parser.parseDistIp(rawContent);
+        parser.parseLine(rawContent);
+        System.out.println(parser.getSourceIp()+":"+parser.getSourcePort());
+        System.out.println(parser.getDestinationIp()+":"+parser.getDestinationPort());
+        System.out.println(parser.getProtocol());
+    }
+
+    public String getSourceIp() {
+        return sourceIp;
+    }
+
+    public String getDestinationIp() {
+        return destinationIp;
+    }
+
+    public String getSourcePort() {
+        return sourcePort;
+    }
+
+    public String getDestinationPort() {
+        return destinationPort;
+    }
+
+    public String getProtocol() {
+        return protocol;
     }
 }
